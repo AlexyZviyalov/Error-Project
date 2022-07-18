ba583f4624f40a9a12f6741ed95bb9f19c01f31b
Aliaksei <53183566+AlexyZviyalov@users.noreply.github.com>
2022-07-18 17:29:56 +0300

    Initial commit

---
+++ Assets/CongratScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratScript : MonoBehaviour
+{
+    public TextMesh Text;
+    public ParticleSystem SparksParticles;
+
+    private List<string> TextToDisplay = new List<string>();
+
+    private float rotatingSpeed;
+    private float TimeToNextText;
+
+    private int CurrentText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeToNextText = 0.0f;
+        CurrentText = 0;
+        
+        rotatingSpeed = 1.0f;
+
+        List<string[]> textToDisplay = new List<string[]>();
+
+        TextToDisplay.Add("Congratulation");
+        TextToDisplay.Add("All Errors Fixed");
+
+        Text.text = TextToDisplay[CurrentText];
+        
+        SparksParticles.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeToNextText += Time.deltaTime;
+
+        if (TimeToNextText > 1.5f)
+        {
+            TimeToNextText = 0.0f;
+            
+            CurrentText++;
+           
+            if (CurrentText >= TextToDisplay.Count)
+            {
+                CurrentText = 0;
+                
+            }
+        }
+        Text.text = TextToDisplay[CurrentText];
+        Text.transform.Rotate(0, rotatingSpeed, 0, Space.Self);
+    }
+
+}
